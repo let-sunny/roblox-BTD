cb70781e799fc3ae48e4f5d71233409d1e0091b4
Sunny <sunny.dev.js@gmail.com>
2021-12-24 15:13:57 +0900

    Player - 시작시 카메라 설정

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,1 +1,8 @@
-print("Hello world, from client!")
+local runService = game:GetService("RunService")
+runService.Heartbeat:Wait()
+
+local player = game:GetService("Players").LocalPlayer
+
+player.CameraMode = Enum.CameraMode.Classic
+player.CameraMaxZoomDistance = 20
+player.CameraMinZoomDistance = 2
