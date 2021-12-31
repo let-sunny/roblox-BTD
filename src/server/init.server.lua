1ce7887f7cd8f5ef177c4cfb248264417f526e75
Sunny <sunny.dev.js@gmail.com>
2021-12-31 23:45:02 +0900

    server script - datastore 써보기용 leaderstats 사용

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,17 @@
-print("Hello world, from server!")
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage").Common
+local StageStore = require(ReplicatedStorage:WaitForChild("StageStore"))
+
+local function leaderboardSetup(player)
+	local leaderstats = Instance.new("Folder")
+	leaderstats.Name = "leaderstats"
+	leaderstats.Parent = player
+
+    local stage = Instance.new("IntValue")
+    stage.Name = "Stage"
+    stage.Value = StageStore.getMaxStage(player)
+    stage.Parent = leaderstats
+end
+
+-- Connect the "leaderboardSetup()" function to the "PlayerAdded" event
+Players.PlayerAdded:Connect(leaderboardSetup)
